4aa4ac07123406cd390242a85f90ff3add7afe13
epaschke22 <ericksonpaschke@gmail.com>
2021-05-18 18:20:07 +0000

    fixed pep8 issues

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,9 +6,10 @@ import requests
 def number_of_subscribers(subreddit):
     """gets the number of subscribers from a subreddit"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent':'Python:sub.counter:v0.1 (by /u/willy)'}
+    headers = {'User-Agent': 'Python:sub.counter:v0.1 (by /u/willy)'}
     try:
-        subinfo = requests.get(url, headers=headers, allow_redirects=False).json()
+        subinfo = requests.get(url, headers=headers,
+                               allow_redirects=False).json()
         return subinfo['data']['subscribers']
     except:
         return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ import requests
 def top_ten(subreddit):
     """returns top 10 hot posts of a subreddit"""
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    header = {'User-Agent':'Python:sub.counter:v0.1 (by /u/willy)'}
+    header = {'User-Agent': 'Python:sub.counter:v0.1 (by /u/willy)'}
     param = {'limit': 10}
     try:
         subtop10 = requests.get(url, headers=header, params=param,
