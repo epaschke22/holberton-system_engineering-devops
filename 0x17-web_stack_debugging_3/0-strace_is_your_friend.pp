e228410ec568496641651347332d30e7cb97041a
epaschke22 <ericksonpaschke@gmail.com>
2021-05-20 03:20:53 +0000

    updating task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,4 @@
 # fixes small misspelling in apache config
 exec { 'modify_file':
-    command => "sudo sed -i 's/phpp/php/g'"
-    path    => '/var/www/html/wp-settings.php',
+    command => "sudo sed -i 's/phpp/php/g' /var/www/html/wp-settings.php"
 }
